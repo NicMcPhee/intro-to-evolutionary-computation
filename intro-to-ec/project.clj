ceadb40002f95a7a7e33258e9b46acf1eaa81343
Nic McPhee <mcphee@morris.umn.edu>
2019-09-06 14:04:24 -0500

    Add `proto-repl` dependency
    This is necessary to make things work nice in atom.

--- intro-to-ec/project.clj
+++ intro-to-ec/project.clj
@@ -1,18 +1,19 @@
 (defproject intro-to-ec "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/math.combinatorics "0.1.6"]]
-  :plugins [[jonase/eastwood "0.3.6"]
-            [lein-cljfmt "0.6.4"]
-            [lein-kibit "0.1.7"]]
-  :aliases {"lint" ^{:doc "Execute cljfmt check, eastwood, and kibit"}
-            ["do" ["cljfmt" "check"]
-                  ["eastwood" "{:source-paths [\"src\"]}"]
-                  ["kibit"]]}
-  :main ^:skip-aot intro-to-ec.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
+            :description "FIXME: write description"
+            :url "http://example.com/FIXME"
+            :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+                      :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :dependencies [[org.clojure/clojure "1.10.0"]
+                           [org.clojure/math.combinatorics "0.1.6"]
+                           [proto-repl "0.3.1"]]
+            :plugins [[jonase/eastwood "0.3.6"]
+                      [lein-cljfmt "0.6.4"]
+                      [lein-kibit "0.1.7"]]
+            :aliases {"lint" ^{:doc "Execute cljfmt check, eastwood, and kibit"}
+                      ["do" ["cljfmt" "check"]
+                       ["eastwood" "{:source-paths [\"src\"]}"]
+                       ["kibit"]]}
+            :main ^:skip-aot intro-to-ec.core
+            :target-path "target/%s"
+            :profiles {:uberjar {:aot :all}
+                       :dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
