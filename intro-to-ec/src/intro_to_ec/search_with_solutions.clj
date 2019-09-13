9272dad481484af66314eb1547c7fc2ae32a8045
Nic McPhee <mcphee@morris.umn.edu>
2019-09-13 17:32:27 -0500

    Extract path from solution
    This adds a new `generate-path` function that we can use to extract
    the solution path from the `came-from` map.
    
    I also renamed `visited` to `came-from` since that name seemed to
    better reflect what it was doing. I also moved the `let` for `kids` down
    below the early exit checks because it might be expensive to generate
    the child nodes in some circumstances, and we wouldn't want to do that
    if it wasn't necessary.

--- intro-to-ec/src/intro_to_ec/search_with_solutions.clj
+++ intro-to-ec/src/intro_to_ec/search_with_solutions.clj
@@ -17,24 +17,31 @@
   {:get-next-node rand-nth
    :add-children concat})
 
+(defn generate-path
+  [came-from node]
+  (if (= :start-node (get came-from node))
+    [node]
+    (conj (generate-path came-from (get came-from node)) node)))
+
 (defn search
   [{:keys [get-next-node add-children]}
    {:keys [goal? make-children]}
    start-node max-calls]
- (loop [frontier [start-node]
-        visited {}
-        num-calls 0]
-  (println num-calls ": " frontier)
-  (println visited)
-  (let [current-node (get-next-node frontier)
-        kids (remove-previous-states (make-children current-node) frontier (keys visited))]
-    (cond
-      (goal? current-node) current-node
-      (= num-calls max-calls) :max-calls-reached
-      :else
-      (recur
-       (add-children
-        kids
-        (rest frontier))
-       (reduce (fn [vs child] (assoc vs child current-node)) visited kids)
-       (inc num-calls))))))
+  (loop [frontier [start-node]
+         came-from {start-node :start-node}
+         num-calls 0]
+    (println num-calls ": " frontier)
+    (println came-from)
+    (let [current-node (get-next-node frontier)]
+      (cond
+        (goal? current-node) (generate-path came-from current-node)
+        (= num-calls max-calls) :max-calls-reached
+        :else
+        (let [kids (remove-previous-states
+                    (make-children current-node) frontier (keys came-from))]
+          (recur
+           (add-children
+            kids
+            (rest frontier))
+           (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
+           (inc num-calls)))))))
