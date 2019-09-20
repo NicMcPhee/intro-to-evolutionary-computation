d17c24d31f77a42ea3a7d611fdf41d701fafc06e
Jackson Tenor <tenor031@umn.edu>
2019-09-20 15:21:02 -0500

    Outline A* functions End of Friday class

--- intro-to-ec/src/intro_to_ec/heuristic_search.clj
+++ intro-to-ec/src/intro_to_ec/heuristic_search.clj
@@ -3,29 +3,50 @@
   (:require [clojure.data.priority-map :as pm])
   (:require [intro-to-ec.search-with-solutions :as sws]))
 
-
+"Bad legacy code ;)"
 (def depth-first-search
   {:get-next-node first
     :add-children concat})
 
+"Heuristic Search"
 (defn distance-heuristic
   [node goal]
   (+ (Math/abs (- (first node) (first goal))) (Math/abs (- (second node) (second goal)))))
 
-(defn add-priority
+(defn add-heuristic-priority
   [kid goal] [kid (distance-heuristic kid goal)])
-(defn add-priority-map
-  [kids goal] (map (fn [kid] (add-priority kid goal)) kids))
+(defn add-heuristic-priority-map
+  [kids goal] (map (fn [kid] (add-heuristic-priority kid goal)) kids))
 
 (defn reduce-frontier-distance
   [frontier kids goal]
-  (into frontier (add-priority-map kids goal)))
-"We need to turn kids into [kids priority] to pass into where we have kids in reduce-frontier-distance"
+  (into frontier (add-heuristic-priority-map kids goal)))
 
 (def distance-search
   {:get-next-node (fn [pmap](first (first pmap)))
    :add-children reduce-frontier-distance
    })
+
+"A*"
+
+(defn a*-heuristic
+  [node goal steps]
+  (+ steps (Math/abs (- (first node) (first goal))) (Math/abs (- (second node) (second goal)))))
+
+(defn add-a*-priority
+    [kid goal steps] [kid (a*-heuristic kid goal steps)])
+(defn add-a*-priority-map
+  [kids goal steps] (map (fn [kid] (add-a*-priority kid goal steps)) kids))
+
+(defn reduce-frontier-distance-a*
+  [frontier kids goal steps]
+  (into frontier (add-a*-priority-map kids goal steps)))
+
+"TODO: change the 1 to the number of steps when you pass it in"
+(def a*-search
+  {:get-next-node (fn [pmap](first (first pmap)))
+   :add-children reduce-frontier-distance-a*
+  })
 
 (defn is-goal?
    [location goal]
@@ -36,6 +57,13 @@
     (if (= :start-node (get came-from node))
       [node]
       (conj (generate-path came-from (get came-from node)) node)))
+
+"The error is somewhere around here."
+(defn get-path-length
+  [came-from node len]
+  (if (= :start-node (get came-from node))
+    1
+    (+ 1 (get-path-length came-from (get came-from node) len))))
 
 (defn remove-previous-states
     [new-states frontier visited]
@@ -66,8 +94,9 @@
                     (keys came-from))]
           (recur
            (add-children
-            (dissoc frontier start-node)
+            (dissoc frontier current-node)
             kids
-            goal)
+            goal
+            (get-path-length start-node current-node 0))
            (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
            (inc num-calls)))))))
