e0b9029ec12acb22bc8d734c35c4553a2bf042e4
Jackson Tenor <tenorjackson@gmail.com>
2019-11-24 02:47:56 -0600

    Write search and heuristic functions for 2048

--- intro-to-ec/src/intro_to_ec/2048/search.clj
+++ intro-to-ec/src/intro_to_ec/2048/search.clj
@@ -1,30 +1,47 @@
 (ns intro-to-ec.2048.search
   (:require [intro-to-ec.2048.game :as game]))
 
+
+(defn has-2048?
+  [board]
+  (not (= (.indexOf board 2048) -1)))
+
+(defn score
+  [board]
+  (apply + board))
+
+(defn count-zeros
+  [board]
+  (count (filter zero? board)))
+
+"Stolen"
+(defn remove-visited
+  [new-states frontier visited]
+  (remove (cset/union (set frontier) (set visited)) new-states))
+
+(defn generate-path
+  [came-from node]
+  (if (= :start-node (get came-from node))
+    [node]
+    (conj (generate-path came-from (get came-from node)) node)))
+
 (defn search
-  [{:keys [get-next-node add-children]}
+  [{:keys [get-next add-children]}
    {:keys [goal? make-children]}
-   start-node max-calls]
-  (loop [frontier [start-node]
-         came-from {start-node :start-node}
+   start-state max-calls]
+  (loop [frontier [start-state]
+         came-from {start-state :start}
          num-calls 0]
     (println num-calls ": " frontier)
     (println came-from)
-    (let [current-node (get-next-node frontier)]
+    (let [current (get-next frontier)]
       (cond
-        (goal? current-node) (generate-path came-from current-node)
+        (goal? current) (generate-path came-from current)
         (= num-calls max-calls) :max-calls-reached
         :else
-        (let [kids (remove-previous-states
-                    (make-children current-node) frontier (keys came-from))]
+        (let [kids (remove-visited (make-children current) frontier (keys came-from))]
           (recur
-           (add-children
-            kids
-            (rest frontier))
-           (reduce (fn [cf child] (assoc cf child current-node)) came-from kids)
-           (inc num-calls)))))))
-
-
-(defn has-2048?
- []
- false)
+           (add-children kids (rest frontier))
+           (reduce (fn [cf child] (assoc cf child current)) came-from kids)
+           (inc num-calls)
+           ))))))
